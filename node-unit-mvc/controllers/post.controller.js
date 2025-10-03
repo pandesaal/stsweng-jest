a31b466162d983810a708a25671d373266bd5581
Kyle Gan <kyle_elijah_gan@dlsu.edu.ph>
2025-10-03 23:36:44 +0800

    update post implementation

--- node-unit-mvc/controllers/post.controller.js
+++ node-unit-mvc/controllers/post.controller.js
@@ -1,27 +1,31 @@
-const PostModel = require('../models/post.model');
-const PostController = {};
+const mongoose = require('./connection');
 
-PostController.create = (req, res) => {
-    return PostModel.createPost(req.body, (err, post) => {
-        if (err) {
-            return res.status(500).end();
-        } else {
-            return res.json(post);
-        }
-    })
+const postSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    date: { type: Date, default: Date.now },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true }
+  }
+);
 
-};
+const Post = mongoose.model('posts', postSchema);
 
-PostController.update = (req, res) => {
+exports.createPost = (obj, next) => {
+    const post = new Post(obj);
 
-};
+    post.save(function(err, post) {
+        next(err, post)
+    }) 
+}
 
-PostController.findPost = (req, res) => {
+exports.updatePost = (id, obj, next) => {
+    Post.findByIdAndUpdate(id, obj, { new: true }, function(err, post) {
+        next(err, post);
+    });
+}
 
-};
-
-PostController.getAllPosts = (req, res) => {
-
-};
-
-module.exports = PostController;
+exports.findPost = (id, next) => {
+    Post.findById(id, function(err, post) {
+        next(err, post);
+    });
+}
